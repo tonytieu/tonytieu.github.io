bd77f2f0c6b55ebff61851d19bc45e53e13ada89
tony <tony@thesoftwarepractice.com>
2020-03-02 17:19:27 +0700

    change

--- assets/plugins/wizard/steps.js
+++ assets/plugins/wizard/steps.js
@@ -1,16 +1,16 @@
-$(".tab-wizard").steps({
-    headerTag: "h6"
-    , bodyTag: "section"
-    , transitionEffect: "fade"
-    , titleTemplate: '<span class="step">#index#</span> #title#'
-    , labels: {
-        finish: "Submit"
-    }
-    , onFinished: function (event, currentIndex) {
-       swal("Form Submitted!", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lorem erat eleifend ex semper, lobortis purus sed.");
+// $(".tab-wizard").steps({
+//     headerTag: "h6"
+//     , bodyTag: "section"
+//     , transitionEffect: "fade"
+//     , titleTemplate: '<span class="step">#index#</span> #title#'
+//     , labels: {
+//         finish: "Submit"
+//     }
+//     , onFinished: function (event, currentIndex) {
+//        swal("Form Submitted!", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lorem erat eleifend ex semper, lobortis purus sed.");
             
-    }
-});
+//     }
+// });
 
 
 var form = $(".validation-wizard").show();
